f18a4142f0238808e7919a86cb27efe3bbd286cf
Trenton Teasdale <dr.teasdale.trenton@gmail.com>
2022-06-11 18:49:52 -0600

    vp update

--- src/bio.js
+++ src/bio.js
@@ -1,9 +1,16 @@
 import img from './images/icon.png';
+import vpImg from './images/vpImg.png';
 
 export const bio = () => {
-    let bio = document.getElementById('bios');
+    let bio = document.getElementById('presBio');
     const donatoImg = new Image();
     donatoImg.src = img;
 
     bio.prepend(donatoImg);
+
+    let vp = document.getElementById('vp');
+    const viceImg = new Image();
+    viceImg.src = vpImg;
+
+    vp.prepend(viceImg);
 }
